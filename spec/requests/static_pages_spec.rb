32e47ce3a0ecce5288cea3648eab5ffe3f20b58f
Timmy Martin <timmywmartin@gmail.com>
2014-01-15 23:07:28 -0700

    Adding a full featured testing suite

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -12,7 +15,7 @@ describe "Static pages" do
 
    it "should have the title." do
    	visit '/static_pages/home' 
-   	expect(page).to have_title ("Ruby on Rails Tutorial Sample App | Home ")
+   	expect(page).to have_title ("#{base_title} | Home ")
 	end
    end
 
@@ -27,9 +30,9 @@ it "should have the content 'Help' " do
 
  it "should have the title." do
    	visit '/static_pages/help' 
-   	expect(page).to have_title ("Ruby on Rails Tutorial Sample App | Help")
+   	expect(page).to have_title ("#{base_title} | Help")
 	end
-   end
+  end
 
 describe "About Page" do
 
@@ -41,7 +44,21 @@ it "should have the content 'About' " do
 
  it "should have the title." do
    	visit '/static_pages/about'
-   	expect(page).to have_title ("Ruby on Rails Tutorial Sample App | About")
+   	expect(page).to have_title ("#{base_title} | About")
 	end
   end
+
+  describe "Contact Page" do
+
+    it "should have the content 'Contact'" do
+    visit '/static_pages/contact'
+    expect(page).to have_content('Contact')
+  end
+
+  it "should have the title 'Contact' " do
+  visit '/static_pages/contact'
+  expect(page).to have_title(" #{base_title} | Contact ")
+  end
+  end
+
 end
